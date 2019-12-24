999fbb7256d5bd72372d13557f4442e49390268e
woowahanlee <leesh@woowahan.com>
2019-12-24 13:47:01 +0900

    api module init

---
+++ aggreement-support-api/src/main/java/com/kakaopay/api/AggreementSupportApiApplication.java
@@ -0,0 +1,12 @@
+package com.kakaopay.api;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class AggreementSupportApiApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(AggreementSupportApiApplication.class)
+                .run(args);
+    }
+}
