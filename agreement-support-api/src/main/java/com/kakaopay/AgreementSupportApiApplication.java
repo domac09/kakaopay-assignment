54c36fd227a7f682ef5ca720fab490fed82e21d3
woowahanlee <leesh@woowahan.com>
2019-12-24 14:33:03 +0900

    datasource add.

--- agreement-support-api/src/main/java/com/kakaopay/AgreementSupportApiApplication.java
+++ agreement-support-api/src/main/java/com/kakaopay/AgreementSupportApiApplication.java
@@ -1,4 +1,4 @@
-package com.kakaopay.api;
+package com.kakaopay;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
